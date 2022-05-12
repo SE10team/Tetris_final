1d17659dd781de282aa8cab151a6098214b98300
yubin <dbqls2821@gmail.com>
2022-05-12 16:08:45 +0900

    유빈_feat_3_대전모드에서 추가 모드 선택 화면

--- src/seoultech/se/tetris/startScreen/FightModeSelectionMenuScreen.java
+++ src/seoultech/se/tetris/startScreen/FightModeSelectionMenuScreen.java
@@ -1,2 +1,74 @@
-package seoultech.se.tetris.startScreen;public class FightModeSelectionMenuScreen {
+package seoultech.se.tetris.startScreen;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FightModeSelectionMenuScreen extends JPanel {
+
+  FightModeSelectionScreen screen;
+  private PlayerKeyListener playerKeyListener;
+  JButton[] buttons;
+
+  public FightModeSelectionMenuScreen(FightModeSelectionScreen screen) {
+    this.screen = screen;
+    playerKeyListener = new PlayerKeyListener();
+
+    String[] btnText = {"일반 대전 모드", "아이템 대전 모드", "시간제한 대전 모드", "뒤로가기"};
+    buttons = new JButton[btnText.length];
+
+    for (int i = 0; i < btnText.length; i++) {
+      buttons[i] = new JButton(btnText[i]);
+      add( buttons[i] );
+
+      Set<AWTKeyStroke> set = new HashSet<AWTKeyStroke>( buttons[i].getFocusTraversalKeys(
+        KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS ) );
+      set.add( KeyStroke.getKeyStroke( "UP" ) );
+      buttons[i].setFocusTraversalKeys(
+        KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS, set );
+
+      Set<AWTKeyStroke> set2 = new HashSet<AWTKeyStroke>( buttons[i].getFocusTraversalKeys(
+        KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS ) );
+      set2.add( KeyStroke.getKeyStroke( "DOWN" ) );
+      buttons[i].setFocusTraversalKeys(
+        KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS, set2 );
+
+      buttons[i].addKeyListener(playerKeyListener);
+    }
+  }
+
+
+
+  public class PlayerKeyListener extends Component implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+      if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+        if (e.getSource() == buttons[0]) {
+
+        } else if (e.getSource() == buttons[1]) {
+
+        } else if (e.getSource() == buttons[2]) {
+
+        } else if (e.getSource() == buttons[3]) {
+          screen.setVisible(false);
+          StartScreen startScreen = new StartScreen();
+        }
+
+      } else if (e.getKeyCode() != KeyEvent.VK_ENTER || e.getKeyCode() != KeyEvent.VK_DOWN || e.getKeyCode() != KeyEvent.VK_UP) {
+        JOptionPane.showConfirmDialog(this, "엔터와 위아래 화살표를 눌러주세요", "confirm", JOptionPane.INFORMATION_MESSAGE);
+      }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+  }
 }
--- src/seoultech/se/tetris/startScreen/FightModeSelectionScreen.java
+++ src/seoultech/se/tetris/startScreen/FightModeSelectionScreen.java
@@ -1,2 +1,49 @@
-package seoultech.se.tetris.startScreen;public class FightModeSelectionScreen {
+package seoultech.se.tetris.startScreen;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class FightModeSelectionScreen extends JFrame {
+
+  FightModeSelectionMenuScreen menuScreen;
+
+  public FightModeSelectionScreen() {
+    menuScreen = new FightModeSelectionMenuScreen(this);
+
+
+    AbstractAction buttonPressed = new AbstractAction() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand() == "일반 대전 모드") {
+
+        } else if (e.getActionCommand() == "아이템 대전 모드") {
+
+        }  else if (e.getActionCommand() == "시간제한 대전 모드") {
+
+        } else if (e.getActionCommand() == "뒤로가기") {
+          setVisible(false);
+          StartScreen startScreen = new StartScreen();
+        }
+      }
+    };
+
+    setTitle("대전 게임 모드 선택 화면");
+    setSize(400, 500);
+    setResizable(false);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setBackground(Color.PINK);
+    setLayout(null);
+    setLocationRelativeTo(null); // 창 가운데로
+
+    // 메뉴 버튼들
+//    StartScreenMenu startScreenMenu = new StartScreenMenu();
+    menuScreen.setBounds(130, 190, 145, 300);
+    for (int i = 0; i < menuScreen.buttons.length; i++) {
+      menuScreen.buttons[i].addActionListener(buttonPressed);
+    }
+    add(menuScreen);
+
+    setVisible(true);
+  }
 }
