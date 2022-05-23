51f04d20ff98d3ebe9bb02662d61f1ff39e76c78
yubin <dbqls2821@gmail.com>
2022-05-23 18:48:28 +0900

    유빈_feat_3_대전모드 양쪽 보드 KeyListener 추가

--- src/seoultech/se/tetris/GUI/MatchScreen.java
+++ src/seoultech/se/tetris/GUI/MatchScreen.java
@@ -7,6 +7,8 @@ import seoultech.se.tetris.settingScreen.FileInputOutput;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 class Pair {
     Board first;
@@ -37,6 +39,11 @@ public class MatchScreen extends JFrame {
 
     private Pair boards;
 
+    PlayerKeyListener playerKeyListener;
+
+    Board mainBoard1;
+    Board mainBoard2;
+
     public static void main(String[] args) throws Exception {
         MatchScreen tetris = new MatchScreen();
         tetris.setVisible(true);
@@ -59,7 +66,7 @@ public class MatchScreen extends JFrame {
         ScoreBoard scoreBoard1 = new ScoreBoard(score1);
         NextGenerateBlock nextBlock1 = new NextGenerateBlock();
         NextBoard nextBoard1 = new NextBoard(nextBlock1);
-        Board mainBoard1 = new Board(this, score1, scoreBoard1, nextBlock1, nextBoard1);
+        mainBoard1 = new Board(this, score1, scoreBoard1, nextBlock1, nextBoard1);
 
         mainBoard1.setBounds(50, 50, 350, 700);
         scoreBoard1.setBounds(450, 50, 200, 100);
@@ -72,7 +79,7 @@ public class MatchScreen extends JFrame {
         ScoreBoard scoreBoard2 = new ScoreBoard(score2);
         NextGenerateBlock nextBlock2 = new NextGenerateBlock();
         NextBoard nextBoard2 = new NextBoard(nextBlock2);
-        Board mainBoard2 = new Board(this, score2, scoreBoard2, nextBlock2, nextBoard2);
+        mainBoard2 = new Board(this, score2, scoreBoard2, nextBlock2, nextBoard2);
 
         mainBoard2.setBounds(800, 50, 350, 700);
         scoreBoard2.setBounds(1200, 50, 200, 100);
@@ -90,6 +97,14 @@ public class MatchScreen extends JFrame {
         add(waitingBoard2);
 
         boards = new Pair(mainBoard1, mainBoard2);
+
+
+
+        playerKeyListener = new PlayerKeyListener();
+        addKeyListener(playerKeyListener);
+        setFocusable(true);
+        requestFocus();
+
     }
 
     public void sendWaitingLines(Board board) {
@@ -98,4 +113,82 @@ public class MatchScreen extends JFrame {
         boards.getOthers(board).clearWaitingLines();
     }
 
+    public class PlayerKeyListener implements KeyListener {
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            // 오른쪽 보드
+            if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                try {
+                    if(!mainBoard2.isBlockOutOfBounds()) mainBoard2.moveBlockDown();
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                mainBoard2.moveBlockRight();
+            } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                mainBoard2.moveBlockLeft();
+            } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+                mainBoard2.rotateBlock();
+            } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                try {
+                    if(!mainBoard2.isBlockOutOfBounds()){
+                        mainBoard2.dropBlock();
+//                        moveBlockToBackground();
+                    }
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            } else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                mainBoard1.timer.stop();
+                mainBoard2.timer.stop();
+                repaint();
+                mainBoard1.showPopup();
+                mainBoard2.timer.start();
+                mainBoard1.timer.start();
+
+            }
+
+
+
+
+            // 왼쪽 보드
+            else if (e.getKeyCode() == KeyEvent.VK_S) {
+                try {
+                    if(!mainBoard1.isBlockOutOfBounds()) mainBoard1.moveBlockDown();
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            } else if (e.getKeyCode() == KeyEvent.VK_D) {
+                mainBoard1.moveBlockRight();
+            } else if (e.getKeyCode() == KeyEvent.VK_A) {
+                mainBoard1.moveBlockLeft();
+            } else if (e.getKeyCode() == KeyEvent.VK_W) {
+                mainBoard1.rotateBlock();
+            } else if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+                try {
+                    if(!mainBoard1.isBlockOutOfBounds()){
+                        mainBoard1.dropBlock();
+//                        moveBlockToBackground();
+                    }
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
+    }
 }
